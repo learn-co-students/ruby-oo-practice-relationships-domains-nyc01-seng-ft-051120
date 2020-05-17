906c028a61dadf7d75c3f4624e307bf398faf651
ChayaGreisman <chayamgreisman@gmail.com>
2020-05-17 13:33:49 -0700

    Lyft Completed

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+
+
+driver1 = Driver.new("Harry")
+driver2 = Driver.new("Sam")
+
+passenger1 = Passenger.new("Meg")
+passenger2 = Passenger.new("Sara")
+
+ride1 = Ride.new(passenger1, driver1, 500)
+ride2 = Ride.new(passenger2, driver1, 600)
+ride3 = Ride.new(passenger1, driver2, 800)
+ride4 = Ride.new(passenger2, driver2, 100)
+
+binding.pry
+0 #so not last line
